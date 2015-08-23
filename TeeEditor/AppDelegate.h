91ddf4042b6fe26ca09a1e8394dc5e84f447f8be
chrisstyle <opener121@gmail.com>
2015-08-23 13:19:03 +0800

    Init project

---
+++ TeeEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TeeEditor
+//
+//  Created by YanZheng on 8/23/15.
+//  Copyright (c) 2015 fastquick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TeeEditor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TeeEditor
+//
+//  Created by YanZheng on 8/23/15.
+//  Copyright (c) 2015 fastquick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
